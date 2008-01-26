c00758680bf6805829f67b71e178d29fd382ce27
Ramon Roca <ramon.roca@guifi.net>
2008-01-26 18:50:24 +0000

    En Miquel te un english molt patatero ;)

--- drupal52/modules/guifi/firmware/firmware-todo.inc.php
+++ drupal52/modules/guifi/firmware/firmware-todo.inc.php
@@ -7,8 +7,8 @@ function unsolclic_todo($dev) {
   _outln_comment('unsolclic version: '.$version);
   _outln_comment();
   _outln_comment(t("This firmware is under construction or not yet started development."));
-  _outln_comment(t("If you want to collaborate and contribute code to make it work,"));
-  _outln_comment(t("please contact our roster of research and development"));
+  _outln_comment(t("If you want to collaborate and contribute with code to make it work,"));
+  _outln_comment(t("please joint to our development lists at:"));
   _outln_comment(t("(guifi-rdes@llistes.projectes.lafarga.org) and visit our website and learn"));
   _outln_comment(t("how to use the module subversion. (http://guifi.net/ca/svn)."));
   _outln_comment();
