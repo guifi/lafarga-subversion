49be1cc05ee806f53af9fc50a798f550bd93ee3c
Ramon Roca <ramon.roca@guifi.net>
2008-08-17 11:21:15 +0000

    Added MAC query and minor fixes/code cleaning
    Requires menu rebuild.

--- drupal6x/modules/guifi/guifi_tools.inc.php
+++ drupal6x/modules/guifi/guifi_tools.inc.php
@@ -64,53 +64,9 @@ function guifi_tools_ip_search($ipv4 = null) {
   $output .= theme('table',$headers,$rows);
   $output .= theme_pager(null, 50);
   return $output;
-  
-  $output .= '<ul>';
-  $output .= '<li>'.t('IP address %ip/%mask found with id %id at interface %interface',
-    array('%ip'=>$ipv4->ipv4,
-      '%mask'=>$ipv4->netmask,
-      '%id'=>$ipv4->id,
-      '%interface'=>$ipv4->interface_id
-    )).'</li>';
-
-  // interface
-  if (!$interface = db_fetch_object(db_query(
-        "SELECT * FROM {guifi_interfaces} WHERE id='%d'",
-        $ipv4->interface_id))) {
-    $output .= '<li>'.t('IP address is orphan').'</li>';
-    if ($ipv4->netmask == '255.255.255.252')
-      return '</ul>'.$output;
-  }            
-  $output .= '<li>'.t('Interface %id/%rc %type found at device %did/!dname',
-    array('%id'=>$interface->id,
-      '%rc'=>$interface->radiodev_counter,
-      '%type'=>$interface->interface_type,
-      '%did'=>$interface->device_id,
-      '!dname'=>l(guifi_get_hostname($interface->device_id),
-         'guifi/device/'.$interface->device_id)
-    )).'</li>';
-  
-  // device
-  if (!$device = db_fetch_object(db_query(
-        "SELECT * FROM {guifi_devices} WHERE id='%d'",
-        $interface->device_id))) {
-    $output .= t('Interface is orphan');
-    return $output;        
-  }
-  $output .= '<li>'.t('Device %id/!dname, %type found at node %nid/!nname',
-    array('%id'=>$device->id,
-      '!dname'=>l(guifi_get_hostname($device->id),
-         'guifi/device/'.$device->id),
-      '%type'=>$device->type,
-      '%nid'=>$device->nid,
-      '!nname'=>l(guifi_get_nodename($device->nid),
-         'node/'.$device->nid)
-    )).'</li>';   
-     
-       
-  return '</ul>'.$output;
 }
- 
+
+// IP search 
 function guifi_tools_ip_search_form($form_state, $params = array()) {
     
   $form['ipv4'] = array(
@@ -120,8 +76,8 @@ function guifi_tools_ip_search_form($form_state, $params = array()) {
     '#default_value' => $params,
     '#size' => 16,
     '#maxlength' => 16,
-    '#description' => t('Enter a valid ipv4 network address to get the related ' .
-        'information available at the database for it.<br>' .
+    '#description' => t('Enter a valid ipv4 network address or pattern ' .
+        'to get the related information available at the database for it.<br>' .
         'You can use valid SQL wilcards (%), for example, to query all the ' .
         'addresses begining with "10.138.0" you can use "10.138.0%"...'),
     '#weight' => 0,
@@ -130,19 +86,87 @@ function guifi_tools_ip_search_form($form_state, $params = array()) {
   
   return $form;
 }
-
-function guifi_tools_ip_search_form_validate($form,$form_state) { 
-  $item = _ipcalc($form_state['values']['ipv4'],'255.255.255.0');
-  if ($item == -1) {
-    form_set_error('ipv4', t('You must specify a valid ipv4 notation.'));
-  }
-}
  
 function guifi_tools_ip_search_form_submit($form, &$form_state) {
-   drupal_goto('guifi/menu/ip/search/'.$form_state['values']['ipv4']);
+   drupal_goto('guifi/menu/ip/ipsearch/'.$form_state['values']['ipv4']);
    return;    
 }
 
+// MAC Search
+function guifi_tools_mac_search($mac = null) {
+  $output = drupal_get_form('guifi_tools_mac_search_form',$mac);
+  
+  if (is_null($mac))
+    return $output;
+    
+  $output .= '<h2>'.t('Query result for "ipv4 LIKE %ipv4"',
+    array('%ipv4'=>"'".$mac."'")).'</h2>';
+
+  $headers = array(t('mac'),t('interface'),t('device'),t('node'));
+  $sqlm = pager_query('SELECT * FROM {guifi_interfaces} WHERE mac LIKE "'.$mac.'"',50);
+  while ($interface = db_fetch_object($sqlm)) {
+    $row = array();
+    $row[] = $interface->mac;
+    $row[] = $interface->id.'/'.$interface->radiodev_counter.' '.
+      $interface->interface_type;     
+
+    // device
+    if ($device = db_fetch_object(db_query(
+         'SELECT * from {guifi_devices} WHERE id=%d',
+         $interface->device_id))) {
+      $row[] = $device->id.'-'.
+        l($device->nick,'guifi/device/'.$device->id);
+    } else {
+      $row[] = t('Orphan');
+      $rows[] = $row;
+      continue;
+    }
+    
+    // node
+    if ($node = db_fetch_object(db_query(
+         'SELECT id from {guifi_location} WHERE id=%d',
+         $device->nid))) {
+      $node = node_load(array('nid'=>$node->id));
+      $row[] = $node->id.'-'.
+        l($node->title,'node/'.$node->id);
+    } else {
+      $row[] = t('Orphan');
+      $rows[] = $row;
+      continue;
+    }
+
+    $rows[] = $row;
+  }
+  
+  $output .= theme('table',$headers,$rows);
+  $output .= theme_pager(null, 50);
+  return $output;
+}
+ 
+function guifi_tools_mac_search_form($form_state, $params = array()) {
+    
+  $form['mac'] = array(
+    '#type' => 'textfield',
+    '#title' => t('MAC address'),
+    '#required' => true,
+    '#default_value' => $params,
+    '#size' => 20,
+    '#maxlength' => 20,
+    '#description' => t('Enter a valid MAC address or pattern ' .
+        'to get the related information available at the database for it.<br>' .
+        'You can use valid SQL wilcards (%), for example, to query all the MAC ' .
+        'addresses begining with "00:0B" you can use "00:0B%"...'),
+    '#weight' => 0,
+  );
+  $form['submit'] = array('#type' => 'submit','#value'=>t('Get information'));
+  
+  return $form;
+}
+ 
+function guifi_tools_mac_search_form_submit($form, &$form_state) {
+   drupal_goto('guifi/menu/ip/macsearch/'.$form_state['values']['mac']);
+   return;    
+}
 
  
 ?>
