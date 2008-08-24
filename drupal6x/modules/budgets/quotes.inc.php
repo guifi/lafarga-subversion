6763b11ae590e59c0f7b46ce7ed88cf7ff26108b
Ramon Roca <ramon.roca@guifi.net>
2008-08-24 20:03:19 +0000

    Fixed add quote link at suppliers page requires rebuild menus

--- drupal6x/modules/budgets/quotes.inc.php
+++ drupal6x/modules/budgets/quotes.inc.php
@@ -4,8 +4,8 @@
  *
  * function for manage supplier quotes
  */
- 
- function budgets_quote_help($path, $arg) {
+
+function budgets_quote_help($path, $arg) {
   if ($path == 'admin/help#budgets_quote') {
     $txt = 'A quote is whatever component of a service or material which can be' .
         ' selected by users as an item of a budget or proposal';
@@ -13,12 +13,29 @@
     return '<p>'.t($txt,$replace).'</p>';
   }
 }
- 
+
+function budgets_quote_add($provider) {
+  global $user;
+
+  $types = node_get_types();
+  $type = 'supplier_quote';
+  // Initialize settings:
+  $node = array(
+    'uid' => $user->uid,
+    'name' => (isset($user->name) ? $user->name : ''),
+    'type' => $type,
+    'language' => '',
+    'supplier_id' => $provider->id);
+
+  drupal_set_title(t('Create @name', array('@name' => $types[$type]->name)));
+  return drupal_get_form($type .'_node_form', $node);
+}
+
 function budgets_quote_form(&$node) {
   guifi_log(GUIFILOG_TRACE,'function budgets_quote_form()',$node);
-  
+
   $type = node_get_types('type',$node);
-  
+
   if (($type->has_title)) {
     $form['title'] = array(
       '#type' => 'textfield',
@@ -27,7 +44,7 @@ function budgets_quote_form(&$node) {
       '#default_value' => $node->title,
     );
   }
- 
+
   if (isset($node->supplier))
     $form['supplier'] = array(
       '#type' => 'textfield',
@@ -36,7 +53,7 @@ function budgets_quote_form(&$node) {
       '#description' => t('Supplier for this quote.'),
       '#default_value' => $node->supplier,
       '#autocomplete_path'=> 'budgets/js/select-supplier',
-    );  
+    );
   else {
     $suppliers = array();
     $qsup = db_query(
@@ -47,10 +64,10 @@ function budgets_quote_form(&$node) {
       if (!user_access('administer suppliers')) {
         if (budgets_supplier_access('update',$sup->id))
           $suppliers[$sup->id] = $sup->title;
-      } else 
+      } else
         $suppliers[$sup->id] = $sup->title;
     }
-       
+
     $form['supplier_id'] = array(
       '#type' => 'select',
       '#required' => true,
@@ -58,9 +75,9 @@ function budgets_quote_form(&$node) {
       '#description' => t('Supplier for this quote.'),
       '#default_value' => $node->supplier_id,
       '#options' => $suppliers,
-    );  
+    );
   }
-    
+
 
   $form['partno'] = array(
     '#type' => 'textfield',
@@ -70,7 +87,7 @@ function budgets_quote_form(&$node) {
     '#title' => t('Part number'),
     '#description' => t('Part number/Code to identify this quote.'),
     '#default_value' => $node->partno,
-  );  
+  );
 
   $form['cost'] = array(
     '#type' => 'textfield',
@@ -98,16 +115,16 @@ function budgets_quote_form(&$node) {
       $type->body_label,
       $type->min_word_count
     );
-  } 
-    
+  }
+
   return $form;
 }
 
 function budgets_quote_ahah_select_supplier($string){
   $matches = array();
-  
+
   $string = strtoupper(arg(3));
-  
+
   $qry = db_query(
     'SELECT ' .
     '  CONCAT(id, "-", title) str '.
@@ -116,7 +133,7 @@ function budgets_quote_ahah_select_supplier($string){
     '  (CONCAT(id, "-", upper(title)) ' .
     '    LIKE "%'.$string.'%") ' .
     'ORDER BY title');
-    
+
   $c = 0;
   while (($value = db_fetch_array($qry)) and ($c < 50)) {
     $c++;
@@ -146,43 +163,43 @@ function budgets_quote_validate($node, &$form) {
     $count = db_fetch_object(db_query($sql));
     if ($count->partno){
       form_set_error('partno', t('Partno %partno already exists.',
-        array('%partno'=>$element['#value'])));  
+        array('%partno'=>$element['#value'])));
     }
   }
-  
+
   // validate supplier exists
-  if (isset($node->supplier)) { 
+  if (isset($node->supplier)) {
     $sup = explode('-',$node->supplier);
     $node->supplier_id = $sup[0];
     $errfield = 'supplier';
   } else
     $errfield = 'supplier_id';
-    
+
   $count = db_fetch_object(db_query(
     'SELECT count(id) supplier ' .
     'FROM {supplier} ' .
     'WHERE id=%d',
     $node->supplier_id));
-  
+
   if (!$count->supplier)
     form_set_error($errfield, t('Supplier does not exist.'));
-    
+
   // validate is privileged for using this supplier
   if (!budgets_supplier_access('update',$node->supplier_id))
     form_set_error($errfield, t('Only can add quotes to owned suppliers.'));
-  
+
 }
 
 function budgets_quote_access($op, $node, $account = null) {
   global $user;
-  
+
   $node = node_load(array('nid'=>$node->id));
   switch($op) {
     case 'create':
       return user_access('create suppliers',$account);
     case 'update':
       if ($node->type == 'supplier_quote') {
-        if ((user_access('administer suppliers',$account)) 
+        if ((user_access('administer suppliers',$account))
           || ($node->uid == $user->uid)) {
           return TRUE;
         }
@@ -197,21 +214,21 @@ function budgets_quote_access($op, $node, $account = null) {
 }
 
 function budgets_quote_prepare(&$node) {
-  
+
   if (empty($node->expires))
     $node->expires = mktime(0, 0, 0, date("m"),  date("d"),  date("Y")+1);
-  
+
   list(
     $node->arrexpires['year'],
     $node->arrexpires['month'],
     $node->arrexpires['day']) = explode(',',date('Y,n,j',$node->expires));
-  
-  // load a $node->supplier text field in case that is an administrator and 
+
+  // load a $node->supplier text field in case that is an administrator and
   // there is more than 10 possible choices
-  
+
   if (!user_access('administer suppliers'))
     return;
-    
+
   $ns = db_fetch_object(db_query(
     'SELECT count(*) suppliers ' .
     'FROM {supplier}'));
@@ -220,27 +237,27 @@ function budgets_quote_prepare(&$node) {
       $s = node_load(array('nid'=>$node->supplier_id));
       $node->supplier = $s->nid.'-'.$s->title;
     } else
-      $node->supplier = ''; 
+      $node->supplier = '';
 }
 
 function budgets_quote_save($node) {
   global $user;
-  
+
   $to_mail = $user->mail;
   $log = '';
-  
+
   if (isset($node->supplier)) {
     $sup = explode('-',$node->supplier);
     $node->supplier_id = $sup[0];
   }
-  
-  $node->expires = 
+
+  $node->expires =
     mktime(0,0,0,
       $node->arrexpires['month'],
       $node->arrexpires['day'],
       $node->arrexpires['year']
     );
-  
+
   $sid = _guifi_db_sql(
     'supplier_quote',
     array('id'=>$node->nid),
@@ -260,7 +277,7 @@ function budgets_quote_save($node) {
       '%user'=>$user->name));
 
   drupal_set_message($subject);
-  
+
   guifi_notify(
     $to_mail,
     $subject,
@@ -270,16 +287,16 @@ function budgets_quote_save($node) {
 function budgets_quote_insert($node) {
   $node->new = true;
   $node->id = $node->nid;
-  budgets_quote_save($node); 
+  budgets_quote_save($node);
 }
 
 function budgets_quote_delete($node) {
   $node->delete = true;
-  budgets_quote_save($node); 
+  budgets_quote_save($node);
 }
 
 function budgets_quote_update($node) {
-  budgets_quote_save($node); 
+  budgets_quote_save($node);
 }
 
 function budgets_quote_load($node) {
@@ -287,20 +304,20 @@ function budgets_quote_load($node) {
     $k = $node->nid;
   else
     $k = $node;
-        
+
   $node = db_fetch_object(
     db_query("SELECT * FROM {supplier_quote} WHERE id = '%d'", $k));
-    
+
   if (is_null($node->id))
     return false;
-  
+
   return $node;
 }
 
 function budgets_quote_view($node, $teaser = FALSE, $page = FALSE) {
   $node = node_prepare($node, $teaser);
   $supplier = node_load(array('nid'=>$node->supplier_id));
-  
+
   $node->content['refquote'] = array(
     '#value'=> theme_refquote($node,$supplier,$teaser).'<hr>',
     '#weight' => -1,
@@ -308,5 +325,5 @@ function budgets_quote_view($node, $teaser = FALSE, $page = FALSE) {
 
   return $node;
 }
- 
+
 ?>
