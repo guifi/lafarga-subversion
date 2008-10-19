a30abc41729dd4d21ecf8f23515769a1910e3b1b
Ramon Roca <ramon.roca@guifi.net>
2008-10-19 21:27:54 +0000

    New theme "guifi.net" based on garland

--- drupal6x/themes/guifi.net/page.tpl.php
+++ drupal6x/themes/guifi.net/page.tpl.php
@@ -1,16 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xml:lang="<?php print $language ?>" lang="<?php print $language ?>">
+<?php
+// $Id: page.tpl.php,v 1.18 2008/01/24 09:42:53 goba Exp $
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
   <head>
     <title><?php print $head_title ?></title>
     <?php print $head ?>
     <?php print $styles ?>
     <?php print $scripts ?>
-    <style type="text/css" media="print">@import "<?php print base_path() . path_to_theme() ?>/print.css";</style>
     <!--[if lt IE 7]>
-    <style type="text/css" media="all">@import "<?php print base_path() . path_to_theme() ?>/fix-ie.css";</style>
+      <?php print phptemplate_get_ie_styles(); ?>
     <![endif]-->
   </head>
-  <body<?php print phptemplate_body_class($sidebar_left, $sidebar_right); ?>>
+  <body<?php print phptemplate_body_class($left, $right); ?>>
 
 <!-- Layout -->
   <div id="header-region" class="clear-block"><?php print $header; ?></div>
@@ -30,12 +32,13 @@
             $site_fields[] = check_plain($site_slogan);
           }
           $site_title = implode(' ', $site_fields);
-//          $site_fields[0] = '<span>'. $site_fields[0] .'</span>';
-          $site_fields[0] = $site_fields[0];
+          if ($site_fields) {
+            $site_fields[0] = '<span>'. $site_fields[0] .'</span>';
+          }
           $site_html = implode(' ', $site_fields);
 
           if ($logo || $site_title) {
-            print '<h1><a href="'. check_url($base_path) .'" title="'. $site_title .'">';
+            print '<h1><a href="'. check_url($front_page) .'" title="'. $site_title .'">';
             if ($logo) {
               print '<img src="'. check_url($logo) .'" alt="'. $site_title .'" id="logo" />';
             }
@@ -43,9 +46,10 @@
           }
         ?>
         </div>
-
         <?php if (isset($primary_links)) : ?>
-          <?php print theme('links', $primary_links, array('class' => 'links primary-links')) ?>
+          <?php
+            print theme('links', $primary_links, array('class' => 'links primary-links'))
+          ?>
         <?php endif; ?>
         <?php if (isset($secondary_links)) : ?>
           <?php print theme('links', $secondary_links, array('class' => 'links secondary-links')) ?>
@@ -53,35 +57,33 @@
 
       </div> <!-- /header -->
 
-      <?php if ($sidebar_left): ?>
+      <?php if ($left): ?>
         <div id="sidebar-left" class="sidebar">
           <?php if ($search_box): ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
-          <?php print $sidebar_left ?>
+          <?php print $left ?>
         </div>
       <?php endif; ?>
 
       <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
-          <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+          <?php print $breadcrumb; ?>
           <?php if ($mission): print '<div id="mission">'. $mission .'</div>'; endif; ?>
-
           <?php if ($tabs): print '<div id="tabs-wrapper" class="clear-block">'; endif; ?>
-          <?php if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif; ?>
-          <?php if ($tabs): print $tabs .'</div>'; endif; ?>
-
-          <?php if (isset($tabs2)): print $tabs2; endif; ?>
-
-          <?php if ($help): print $help; endif; ?>
-          <?php if ($messages): print $messages; endif; ?>
-          <?php print $content ?>
-          <span class="clear"></span>
+          <?php if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'><br>'. $title .'</h2><br>'; endif; ?>
+          <?php if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif; ?>
+          <?php if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif; ?>
+          <?php if ($show_messages && $messages): print $messages; endif; ?>
+          <?php print $help; ?>
+          <div class="clear-block">
+            <?php print $content ?>
+          </div>
           <?php print $feed_icons ?>
-          <div id="footer"><?php print $footer_message ?></div>
+          <div id="footer"><?php print $footer_message . $footer ?></div>
       </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
 
-      <?php if ($sidebar_right): ?>
+      <?php if ($right): ?>
         <div id="sidebar-right" class="sidebar">
-          <?php if (!$sidebar_left && $search_box): ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
-          <?php print $sidebar_right ?>
+          <?php if (!$left && $search_box): ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
+          <?php print $right ?>
         </div>
       <?php endif; ?>
 
