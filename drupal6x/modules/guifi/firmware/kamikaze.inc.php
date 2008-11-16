7ebc303871729ab4b94b972a729839216af3db6f
Miquel Martos <miquelmartos@gmail.com>
2008-11-16 22:28:09 +0000

    Unsolclic Kamikaze: Fix a bug on a leds.sh script for Nanostation. Thanks Xavier for the patch!

--- drupal6x/modules/guifi/firmware/kamikaze.inc.php
+++ drupal6x/modules/guifi/firmware/kamikaze.inc.php
@@ -626,79 +626,85 @@ quagga:x:51:51:quagga:/tmp/.quagga:/bin/false
 #
 # minimod de la feina del Xavi Martinez :-)
 
-gpioctl dirout 0
-gpioctl dirout 1
-gpioctl dirout 3
-gpioctl dirout 4
-L1=0
-L2=0
-L3=0
-L4=0
+  L1=0
+  L2=0
+  L3=0
+  L4=0
 
 #Iniciem el loop
 
 while [ 1 ] 
-do
+  do
 
 # Pillem la qualitat de l\'enllac
- QUAL=\`awk \'/ath0/ {print \$3}\' /proc/net/wireless\`
+  QUAL=\`awk \'/ath0/ {print \$3}\' /proc/net/wireless\`
 #Li traiem el punt final
- QUAL=\${QUAL%.*}
+  QUAL=\${QUAL%.*}
 # Inicialment, tots a 0
- L1T=0
- L2T=0
- L3T=0
- L4T=0
+  L1T=0
+  L2T=0
+  L3T=0
+  L4T=0
 # Comprobem un a un
- if [ \$QUAL != 0 ]
- then
-   L1T=1
-  if [ \$QUAL -gt 15 ]
-  then
-   L2T=1
+  if [ \$QUAL != 0 ]
+    then
+      L1T=1
+      if [ \$QUAL -gt 15 ]
+        then
+          L2T=1
+      fi
+      if [ \$QUAL -gt 30 ]
+        then
+          L3T=1
+      fi
+      if [ \$QUAL -gt 45 ]
+        then
+          L4T=1
+      fi
   fi
-  if [ \$QUAL -gt 30 ]
-  then
-   L3T=1
-  fi
-  if [ \$QUAL -gt 45 ]
-  then
-   L4T=1
-  fi
- fi # \$QUAL!=0
-
 # Encenem/apaguem LED nomes si hi ha canvi
- if [ \$L1 -ne \$L1T ]
-  then
-   gpioctl clear 0
-   L1=\$L1T
-  else
-   gpioctl set 0
- fi
- if [ \$L2 -ne \$L2T ]
-  then
-   gpioctl clear 1
-   L2=\$L2T
-  else
-   gpioctl set 1
- fi
- if [ \$L3 -ne \$L3T ]
-  then
-   gpioctl clear 3
-   L3=\$L3T
-  else
-   gpioctl set 3
- fi
- if [ \$L4 -ne \$L4T ]
-  then
-   gpioctl clear 4
-   L4=\$L4T
-  else
-   gpioctl set 4
- fi
+  if [ \$L1 -ne \$L1T ]
+    then
+      if [ \$L1T -ne 0 ]
+        then
+          gpioctl set 0
+        else
+          gpioctl clear 0
+      fi
+    L1=\$L1T 
+  fi
+  if [ \$L2 -ne \$L2T ]
+    then
+      if [ \$L2T == 0 ]
+        then
+          gpioctl clear 1
+        else
+          gpioctl set 1
+      fi
+    L2=\$L2T
+  fi
+  if [ \$L3 -ne \$L3T ]
+    then
+      if [ \$L3T == 0 ]
+        then
+          gpioctl clear 3
+        else
+          gpioctl set 3
+      fi
+    L3=\$L3T
+  fi
+  if [ \$L4 -ne \$L4T ]
+    then
+      if [ \$L4T == 0 ]
+        then
+          gpioctl clear 4
+        else
+          gpioctl set 4
+      fi
+    L4=\$L4T
+  fi
 
- sleep 1
-
+  sleep 1
 done
 
 ';
