18db6aacb2e0edaadb61a24703676817654d4b81
Miquel Martos <miquelmartos@gmail.com>
2010-06-26 13:32:05 +0000

    Fixed ticket #53 RouterOS Unsolclic, fixed Masquerade on backbone ip address: 172.25.0.0/16 to 172.16.0.0/12

--- drupal6x/modules/guifi/firmware/mikrotik-routeros.inc.php
+++ drupal6x/modules/guifi/firmware/mikrotik-routeros.inc.php
@@ -516,16 +516,16 @@ function unsolclic_routeros($dev) {
   
   _outln_comment();
   _outln_comment(t('Internal addresses NAT'));
-  _outln(':foreach i in [/ip firewall nat find src-address="172.25.0.0/16"] do={/ip firewall nat remove $i;}');
+  _outln(':foreach i in [/ip firewall nat find src-address="172.16.0.0/12"] do={/ip firewall nat remove $i;}');
   _outln(':foreach i in [/ip firewall nat find src-address="192.168.0.0/16"] do={/ip firewall nat remove $i;}');
   _outln('/ip firewall nat');
   if ($dev->variable[firmware] == 'RouterOSv2.9') {
   _outln(sprintf('add chain=srcnat src-address="192.168.0.0/16" dst-address=!192.168.0.0/16 action=src-nat to-addresses=%s to-ports=0-65535 comment="" disabled=no',$ospf_routerid));
-  _outln(sprintf('add chain=srcnat src-address="172.25.0.0/16" dst-address=!172.25.0.0/16 protocol=!ospf action=src-nat to-addresses=%s to-ports=0-65535 comment="" disabled=no',$ospf_routerid));
+  _outln(sprintf('add chain=srcnat src-address="172.16.0.0/12" dst-address=!172.16.0.0/12 protocol=!ospf action=src-nat to-addresses=%s to-ports=0-65535 comment="" disabled=no',$ospf_routerid));
   }
   if (($dev->variable[firmware] == 'RouterOSv3.x') or ($dev->variable['firmware'] == 'RouterOSv4.0+') or ($dev->variable['firmware'] == 'RouterOSv4.7+')) {
   _outln(sprintf('add chain=srcnat src-address="192.168.0.0/16" dst-address=!192.168.0.0/16 action=src-nat to-addresses=%s comment="" disabled=no',$ospf_routerid));
-  _outln(sprintf('add chain=srcnat src-address="172.25.0.0/16" dst-address=!172.25.0.0/16 protocol=!ospf action=src-nat to-addresses=%s comment="" disabled=no',$ospf_routerid));
+  _outln(sprintf('add chain=srcnat src-address="172.16.0.0/12" dst-address=!172.16.0.0/12 protocol=!ospf action=src-nat to-addresses=%s comment="" disabled=no',$ospf_routerid));
   }
   // BGP
   _outln_comment();
